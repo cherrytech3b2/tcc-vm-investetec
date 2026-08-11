101bcdb8f71958a09c5f0149b066186940b93540
deborahsil <deborahh.silva04@gmail.com>
2026-08-11 17:06:03 -0300

    correcao

--- main.py
+++ main.py
@@ -29,5 +29,8 @@ def register_visitor():
     return render_template('login/register/register_visitor.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+def main():
+   app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
